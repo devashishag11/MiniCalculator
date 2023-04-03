c259750692abab14f665dac4c0cf6ed186e2cca0
Devashish Agarwal <73063420+devashishag11@users.noreply.github.com>
2023-04-03 07:02:43 +0530

    Update MiniCalc.cpp

--- MiniCalc.cpp
+++ MiniCalc.cpp
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int a,b;
+char op;
+cout<<"enter a"<<endl;
+cin>>a;
+cout<<"enter b"<<endl;
+cin>>b;
+cout<<"enter operation"<<endl;
+cin>>op;
+switch(op){
+
+case '+': cout<<(a+b)<<endl;
+break;
+
+case '-': cout<<(a-b)<<endl;
+break;
+case '*': cout<<(a*b)<<endl;
+break;
+case '/': cout<<(a/b)<<endl;
+break;
+case '%': cout<<(a%b)<<endl;
+break;
+default:cout<<"enter a valid argument"<<endl;
+}
+
+    return 0;
+}
+
 
